54361083beddf472bd7b9b943c5cbd280dc751d8
RodriiAndino <andinorodrigo03@gmail.com>
2023-03-09 12:00:08 -0300

    fix: keys

--- src/stripe-keys.js
+++ src/stripe-keys.js
@@ -1,6 +1,4 @@
 export default {
-  public:
-    'pk_test_51Mg6lpAINtgMaOaPUK3OuUNVvkBlae2sg4N4uvFpJ9wMrVRKVjbh626nhe0ZxEmQQvEqiuQXJ72CxiBSp8YT7cvd00QhekmLgX',
-  secret:
-    'sk_test_51Mg6lpAINtgMaOaPydhPV0wSVeraOzBsr9dftecJXbnzqprPICwIcqjc6RPS54ljw6QsW9LHBvtJsGW4QF081uRm002OsgZQnG'
+  public: '-----> YOUR KEYS',
+  secret: '-----> YOUR KEYS'
 }

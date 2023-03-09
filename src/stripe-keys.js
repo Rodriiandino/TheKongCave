4534b3d50d09e848bd2833021d1ef16982579894
RodriiAndino <andinorodrigo03@gmail.com>
2023-03-09 11:56:48 -0300

    template and render code

--- src/stripe-keys.js
+++ src/stripe-keys.js
@@ -1,4 +1,6 @@
 export default {
-  public: '...',
-  secret: '...'
+  public:
+    'pk_test_51Mg6lpAINtgMaOaPUK3OuUNVvkBlae2sg4N4uvFpJ9wMrVRKVjbh626nhe0ZxEmQQvEqiuQXJ72CxiBSp8YT7cvd00QhekmLgX',
+  secret:
+    'sk_test_51Mg6lpAINtgMaOaPydhPV0wSVeraOzBsr9dftecJXbnzqprPICwIcqjc6RPS54ljw6QsW9LHBvtJsGW4QF081uRm002OsgZQnG'
 }

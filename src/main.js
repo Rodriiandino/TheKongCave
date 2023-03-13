20c7eb8585ed1e1b9eed9c55a72c3fa2eefd0981
RodriiAndino <andinorodrigo03@gmail.com>
2023-03-13 13:46:22 -0300

    add: way to pay

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import STRIPE_KEYS from './stripe-keys'
+import { loadStripe } from '@stripe/stripe-js'
 
+const stripe = await loadStripe(STRIPE_KEYS.public)
+
+console.log(stripe)
 const d = document
 const $hero = d.getElementById('hero-gym')
 const $template = d.getElementById('plan-template').content
@@ -10,6 +14,9 @@ const fetchOptions = {
     Authorization: `Bearer ${STRIPE_KEYS.secret}`
   }
 }
+
+const moneyFormat = number =>
+  `$${number.slice(0, -2)}<sup>${number.slice(-2)}</sup> `
 
 Promise.all([
   fetch('https://api.stripe.com/v1/products', fetchOptions),
@@ -32,7 +39,7 @@ Promise.all([
       ${product[0].name}
       </h3>
       <p>
-      ${el.unit_amount_decimal} ${el.currency}
+      ${moneyFormat(el.unit_amount_decimal)} ${el.currency}
       </p>
       <p>
       ${product[0].description}
@@ -50,3 +57,26 @@ Promise.all([
       err.statusText || 'Ocurrio un error al conectarse con la API de Stripe'
     $hero.innerHTML = `<p>Error ${err.status}: ${message}</p>`
   })
+
+d.addEventListener('click', e => {
+  if (e.target.matches('#btn-buy')) {
+    const id = e.target.parentElement.getAttribute('data-price')
+    stripe
+      .redirectToCheckout({
+        lineItems: [
+          {
+            price: id,
+            quantity: 1
+          }
+        ],
+        mode: 'subscription',
+        successUrl:
+          'https://www.youtube.com/watch?v=5KcdQboA7Gc&list=PLy1nL-pvL2M7EUdqARPiayuj5cjGMLCvM&index=3',
+        cancelUrl:
+          'https://www.youtube.com/watch?v=5KcdQboA7Gc&list=PLy1nL-pvL2M7EUdqARPiayuj5cjGMLCvM&index=3'
+      })
+      .then(res => {
+        console.log(res)
+      })
+  }
+})
